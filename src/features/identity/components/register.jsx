d5a26f19c133d0568290acc4a0f4c01ef227286e
Mostafa <noorahmadimustafa204@gmail.com>
2024-08-19 19:20:02 +0430

    building a register form

---
+++ src/features/identity/components/register.jsx
@@ -0,0 +1,134 @@
+import logo from "@assets/images/logo.svg";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { httpServoce } from "../../../core/http-service/http-service";
+
+const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = useForm();
+
+  const onSubmit = (data) => {
+    const { confirmPassword, ...userData } = data;
+    submitForm(userData, { method: "post" });
+  };
+
+  const navigation = useNavigate();
+  const isSubmitting = navigation.state !== "idle";
+  return (
+    <>
+      <div className="text-center mt-4">
+        <img src={logo} style={{ height: "100px" }} />
+        <h1 className="h2">پلتفرم آموزش آنلاین </h1>
+        <p className="lead">
+          جهت ورود لازم است از طریق موبایل و رمز عبور خود اقدام کنید
+        </p>
+        <p className="lead">قبلا ثبت نام نکرده اید ؟</p>
+        <Link to="/login" className="me-2">
+          {" "}
+          وارد شوید{" "}
+        </Link>
+      </div>
+      <div className="card">
+        <div className="card-body">
+          <div className="m-sm-4">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="md-3">
+                <label className="form-label"> موبایل</label>
+                <input
+                  {...register("mobile", {
+                    required: "شماره موبایل الزامی است ",
+                    maxLength: 11,
+                    minLength: 11,
+                  })}
+                  className={`form-control form-control-lg ${
+                    errors.mobile && "is-invalid"
+                  }`}
+                />
+                {errors.mobile && errors.mobile.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.mobile?.message}
+                  </p>
+                )}
+                {errors.mobile &&
+                  (errors.mobile.type === "minLength" ||
+                    errors.mobile.type === "maxLength") && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      موبایل باید 11 رقم باشد
+                    </p>
+                  )}
+              </div>
+              <div className="mb-3">
+                <label className="form-label">رمز عبور</label>
+                <input
+                  {...register("password", { required: "رمز عبور الزامی اس" })}
+                  className={`form-control form-control-lg ${
+                    errors.password && "is-invalid"
+                  }`}
+                  type="password"
+                />
+                {errors.password && errors.password.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.password?.message}
+                  </p>
+                )}
+              </div>
+              <div className="mb-3">
+                <label className="form-label"> تکرار رمز عبور</label>
+                <input
+                  {...register("confirmPassword", {
+                    required: "تکرار رمز الزامی است ",
+                    validate: (value) => {
+                      if (watch("password") !== value) {
+                        return "عدم تطابق با رمز وارد شده ";
+                      }
+                    },
+                  })}
+                  className={`form-control form-control-lg ${
+                    errors.confirmPassword && "is-invalid"
+                  }`}
+                  type="password"
+                />
+                {errors.confirmPassword &&
+                  errors.confirmPassword.type === "required" && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      {errors.confirmPassword?.message}
+                    </p>
+                  )}
+                {errors.confirmPassword &&
+                  errors.confirmPassword.type === "validate" && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      {errors.confirmPassword?.message}
+                    </p>
+                  )}
+              </div>
+              <div className="text-center mt-3">
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="btn btn-lg btn-primary"
+                >
+                  {isSubmitting ? "در حال انجام عملیات " : "ثبت نام کنید "}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Register;
+
+export async function registerAction({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const response = await httpServoce.post("Users", data);
+  return response.status === 200;
+}
