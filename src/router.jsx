1cedbf5b922276247bb54227e39dfe404ea88ad5
Mostafa <noorahmadimustafa204@gmail.com>
2024-08-22 21:33:52 +0430

    Integrate sidebar into MainLayout

--- src/router.jsx
+++ src/router.jsx
@@ -1,21 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
-import Login from "./features/identity/components/login";
-import Register, { registerAction } from "./features/identity/components/register";
+import Login, { loginAction } from "./features/identity/components/login";
+import Register, {
+  registerAction,
+} from "./features/identity/components/register";
 import IdentityLayout from "./layouts/identity-layout";
+import MainLayout from "./layouts/main-layout/main-layout";
+import Courses, { coursesLoader } from "./pages/courses";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        element: <Courses />,
+        index: true,
+        loader: coursesLoader
+      },
+    ],
+  },
   {
     element: <IdentityLayout />,
     children: [
       {
         path: "login",
         element: <Login />,
+        action: loginAction,
+        errorElement: <Login />,
       },
       {
         path: "register",
         element: <Register />,
         action: registerAction,
-        errorElement: <Register/>
+        errorElement: <Register />,
       },
     ],
   },
