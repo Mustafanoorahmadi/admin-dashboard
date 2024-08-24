cba3c0373c0785b609cb62553622bc7bff50cb2e
Mostafa <noorahmadimustafa204@gmail.com>
2024-08-25 00:04:30 +0430

    Implement modal component

--- src/pages/coures-categories.jsx
+++ src/pages/coures-categories.jsx
@@ -1,27 +1,51 @@
 import { Await, defer, useLoaderData } from "react-router-dom";
 import { httpInterceptedService } from "../core/http-service/http-service";
 import CategoryList from "../features/categories/components/category-list";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import Modal from "../components/modal";
 
 const CourseCategories = () => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
   const data = useLoaderData();
   return (
-    <div className="row">
-      <div className="col-12">
-        <div className="d-flex align-items-center justify-content-between mb-5">
-          <a className="btn btn-primary fw-bolder mt-n1">افزودن دسته جدید</a>
+    <>
+      <div className="row">
+        <div className="col-12">
+          <div className="d-flex align-items-center justify-content-between mb-5">
+            <a className="btn btn-primary fw-bolder mt-n1">افزودن دسته جدید</a>
+          </div>
+          <Suspense
+            fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}
+          >
+            <Await resolve={data.categories}>
+              {(loadedCategorise) => (
+                <CategoryList
+                  setShowDeleteModal={setShowDeleteModal}
+                  categories={loadedCategorise}
+                />
+              )}
+            </Await>
+          </Suspense>
         </div>
-        <Suspense
-          fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}
-        >
-          <Await resolve={data.categories}>
-            {(loadedCategorise) => (
-              <CategoryList categories={loadedCategorise} />
-            )}
-          </Await>
-        </Suspense>
       </div>
-    </div>
+      <Modal
+        isOpen={showDeleteModal}
+        open={setShowDeleteModal}
+        title="حذف"
+        body="آیا از حذف این دسته اطمینان دارید ؟"
+      >
+        <button
+          type="button"
+          className="btn btn-secondary fw-bolder"
+          onClick={() => setShowDeleteModal(false)}
+        >
+          انصراف
+        </button>
+        <button type="button" className="btn btn-primary fw-bolder">
+          حذف
+        </button>
+      </Modal>
+    </>
   );
 };
 
@@ -33,7 +57,7 @@ export async function categoriesLoader({ request }) {
 
 const loadCategories = async (request) => {
   const page = new URL(request.url).searchParams.get("page") || 1;
-  const pageSize = import.meta.env.VITE_PAGE_SIZE ;
+  const pageSize = import.meta.env.VITE_PAGE_SIZE;
   let url = "/CourseCategory/sieve";
 
   url += `?page=${page}&pageSize=${pageSize}`;
